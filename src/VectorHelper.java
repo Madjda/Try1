bd719d8d64d13274a88909c4041bf5e8d449f271
laptop dell <laptop dell@TimaAb>
2017-10-15 18:21:23 +0200

    ThirdCommit

--- src/VectorHelper.java
+++ src/VectorHelper.java
@@ -1,6 +1,34 @@
 import java.lang.reflect.Array;
 
 public class VectorHelper {
-	private Array [] vect ;
+	  private  Array[] vect;
+      private long tail;
+	
+      
+      public VectorHelper( long tail) {
+		super();
+		
+		this.setTail(tail);
+		this.setVect(new Array[(int) tail]);
+	}
 
+
+	public Array[] getVect() {
+		return vect;
+	}
+
+
+	public void setVect(Array[] vect) {
+		this.vect = vect;
+	}
+
+
+	public long getTail() {
+		return tail;
+	}
+
+
+	public void setTail(long tail) {
+		this.tail = tail;
+	}
 }
