fd589a997fe3c844e04a64a38487a0cac3379ac0
laptop dell <laptop dell@TimaAb>
2017-10-16 00:51:31 +0200

    6th commit

--- src/VectorHelper.java
+++ src/VectorHelper.java
@@ -15,11 +15,25 @@ public class VectorHelper {
 
        public void CreateVectRand (){
     	Random rand=new Random();
-    	int tail =rand.nextInt(100);
+    	int tail =rand.nextInt(10);
     	
        	for (int i=0;i<tail;i++){
        		vect.add(rand.nextInt(750));
        	}
+       }
+       
+       public int FindSmlElt (final int born){
+    	   int min;
+   
+    	   if (born!=this.getTail()){
+    	   min =born;
+    	   for (int i=born+1;i<this.getTail();i++){
+    		 if (vect.get(i)<vect.get(min) ) min=i;
+    	   }
+    	   return min;
+    	   }
+    	   else return -1;
+    	   
        }
       
        public void CreateVect (long tail, int init,int pas){
@@ -47,42 +61,42 @@ public class VectorHelper {
 	public void setTail(long tail) {
 	}
 	
-	public ArrayList<Integer> Sommer( ArrayList<Integer> vectr )
+	public VectorHelper Sommer( VectorHelper vectr )
 	{ 
-		ArrayList<Integer>  vectSomme;
+		VectorHelper vectSomme =new VectorHelper();
 	
-     Try {
+        try {
     	     if (vectr.getTail() != this.getTail() ) 
                 {
-    	            new TailleException () ;
+    	            throw new TailleException () ;
     	 
                  } else  {
                 	  
                 	 for (int i=0;i<this.getTail();i++) {
-                		 vectSomme[i]=	vectr[i]+vect[i] ;
-             		                                    } 
-                	 
+                		 vectSomme.vect.add(vectr.vect.get(i)+vect.get(i)) ;
+             		                 } 
                          }
     	 
         }
-     catch (TailleException e) { system.out.print("Impossible de sommer des vecteurs de différentes tailles")}
+     catch (TailleException e) 
+        { System.out.print("Impossible de sommer des vecteurs de différentes tailles");}
      return vectSomme ;
        
     }
 	public void Inverse ()
 	{ 
-	  int x ,i =0,j=this.getTail();
-		while ( i!=j )
-		{  x= this.vect[i];
-		 this.vect[i]=this.vect[j] ;
-		 this.vect[j]=x;
+	  int x ,i =0,j=(int) (this.getTail()-1);
+		while ( i<j )
+		{  x= vect.get(i);
+		 vect.set(i, vect.get(j)) ;
+		 vect.set(j, x) ;
 		 j--;
 		 i++ ;
 		}
-		
+	}
 	
 		
-	}
+	
 
     	
 	
